b718f74847b14874db54ffd495026485d0788355
Fabio Chen <59851467+fabioyebchen@users.noreply.github.com>
2023-08-20 21:50:31 -0400

    fIX cORS AND PROFILE

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import BooksController from './controllers/books/books-controller.js';
 import AuthController from './controllers/users/auth-controller.js';
 import UsersController from './controllers/users/users-controller.js';
 import mongoose from 'mongoose';
-const DB_CONNECTION_STRING= 'mongodb+srv://chenfab:p2JTkzCQSZ7CJQH3@tuiter.x4bhfzh.mongodb.net/?retryWrites=true&w=majority'
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING
 
 mongoose.connect(DB_CONNECTION_STRING)
 
@@ -17,20 +17,10 @@ app.use(
         origin: process.env.FRONTEND_URL,
     })
 );
-
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
 const sessionOptions = {
     secret: "any string",
     resave: false,
-    saveUninitialized: false,
+    saveUnititialized: false,
 };
 if (process.env.NODE_ENV === "development") {
     sessionOptions.proxy = true;
@@ -44,7 +34,6 @@ app.use(
 );
 app.use(express.json());
 BooksController(app);
-UsersController(app);
 AuthController(app);
 UsersController(app);
 app.listen(process.env.PORT || 4000);
